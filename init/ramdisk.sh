0dc6dff7f083d9a300e2dfb98907791d1ead6efd
hidek84 <hidek84@gmail.com>
2020-01-27 23:42:41 +0900

    update ramdisk script

--- init/ramdisk.sh
+++ init/ramdisk.sh
@@ -1,14 +1,25 @@
 # http://qiita.com/snaka/items/c94384f22c7d0b79b035
 # https://www.tekrevue.com/tip/how-to-create-a-4gbs-ram-disk-in-mac-os-x/
 
+# Argument check
+if [ -z "$1" ]
+  then
+    echo "Please input desired size for ramdisk (MB)"
+    exit 1
+fi
+
 # Variables
-desired_mb="128"
+desired_mb="$1"
 disksize=`expr $desired_mb \* 2048`
 DISK_NAME="RamDisk"
 
+# Wait for /Volumes available
+# https://apple.stackexchange.com/questions/58811/how-to-mount-a-ram-disk-on-startup
+sleep 10
+
 # Create RamDisk
 if [ ! -e /Volumes/$DISK_NAME/ ]; then
-  echo "creating Ram Disk"
+  echo "Creating Ram Disk ($desired_mb MB)"
   diskutil erasevolume HFS+ "$DISK_NAME" `hdiutil attach -nomount ram://$disksize`
 else
   echo "Ram Disk already exists"
---
+++ init/startup_ramdisk.sh
@@ -0,0 +1,42 @@
+LABEL_NAME=com.hide.ramdisk
+PLIST_FILE=/Library/LaunchDaemons/$LABEL_NAME.plist
+
+# Input desired size in MB
+echo "Please input the desired size in MB for ramdisk."
+read desired_mb
+
+if ! [[ "$desired_mb" =~ ^[0-9]+$ ]]; then
+    echo "ERROR: Integers only!"
+    exit 1
+fi
+
+# Set up startup script
+echo "Adding startup script. You may be asked the password."
+echo
+
+sudo tee $PLIST_FILE <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" 
+        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>com.hide.ramdisk</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>/bin/sh</string>
+            <string>$HOME/.dotfiles/init/ramdisk.sh</string>
+            <string>$desired_mb</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+    </dict>
+</plist>
+EOF
+
+sudo chown root:wheel $PLIST_FILE
+sudo launchctl unload $PLIST_FILE
+sudo launchctl load $PLIST_FILE
+
+echo "\nRegistered as a startup"
+sudo launchctl list | grep $LABEL_NAME
