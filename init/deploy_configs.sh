82e06765783b883d54ee346e3fba8d8b65311cab
hidek84 <hidek84@gmail.com>
2020-04-04 15:06:15 +0900

    add deploy config script

---
+++ init/deploy_configs.sh
@@ -0,0 +1,9 @@
+KARABINER_CONF=$HOME/.config/karabiner/karabiner.json
+
+echo "Processing for $KARABINER_CONF"
+if [ -d "$(dirname $KARABINER_CONF)" ]; then
+    echo "> Taking a backup"
+    mv $KARABINER_CONF $KARABINER_CONF.bk
+    echo "> Coping config"
+    cp  $HOME/.dotfiles/conf/karabiner.json $KARABINER_CONF
+fi
