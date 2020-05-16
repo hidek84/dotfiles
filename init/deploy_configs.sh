704c28253bbcbc8f7fb690a0b3a5f229838f3aef
hidek84 <hidek84@gmail.com>
2020-05-17 00:47:41 +0900

    fix loading

--- init/deploy_configs.sh
+++ init/deploy_configs.sh
@@ -2,7 +2,7 @@ KARABINER_CONF=$HOME/.config/karabiner/karabiner.json
 
 echo "Processing for $KARABINER_CONF"
 if [ -d "$(dirname $KARABINER_CONF)" ]; then
-    echo "> Taking a backup"
+    echo "> Taking a backup for $KARABINER_CONF with .bk"
     mv $KARABINER_CONF $KARABINER_CONF.bk
     echo "> Coping config"
     cp  $HOME/.dotfiles/conf/karabiner.json $KARABINER_CONF
