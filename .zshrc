6953e7cb819f608a4ebfbf3d044acd6e9cf0db7c
hidek84 <hidek84@gmail.com>
2021-05-19 16:54:41 +0900

    switch from nvm to n for node.js management

--- .zshrc
+++ .zshrc
@@ -43,3 +43,5 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
 
+
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
--- init/nodejs.sh
+++ init/nodejs.sh
@@ -1,25 +1,10 @@
 #!/bin/bash -ex
 
-# https://qiita.com/ffggss/items/94f1c4c5d311db2ec71a
+# https://github.com/tj/n#third-party-installers
 
-source .pathrc
-
-### Install or Update nvm
-if [ -e $NVM_PATH ]; then
-	echo "Already exist $NVM_PATH."
-	echo "Updating to the latest version."
-	cd "$NVM_PATH"
-	git fetch --tags origin
-	git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+if [ -e "$N_PREFIX" ]; then
+    echo "n is already installed in ${N_PREFIX}"
 else
-	echo "Installing into $NVM_PATH"
-	git clone https://github.com/creationix/nvm.git "$NVM_PATH"
-	cd "$NVM_PATH"
-	git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+    echo "Installing n for node.js version management"
+    curl -L https://git.io/n-install | bash
 fi
-
-. "$NVM_PATH/nvm.sh"
-
-### Install or Update Node.js
-nvm install --lts
-nvm alias default lts/*
